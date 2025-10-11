ac7ec749eb50a117cd76c8448eead9bb2880bdd1
Giorgio Maggiolo <giorgio@maggiolo.net>
2025-10-11 08:00:12 +0000

    added autoremove && autoclean

--- .devcontainer/postStart_public.sh
+++ .devcontainer/postStart_public.sh
@@ -3,3 +3,5 @@ pipx install mkdocs-material[imaging] --include-deps
 sudo apt-get update
 sudo apt-get -y install libcairo2-dev libfreetype6-dev libffi-dev libjpeg-dev libpng-dev libz-dev
 sudo apt-get -y dist-upgrade
+sudo apt-get -y autoremove
+sudo apt-get -y autoclean
